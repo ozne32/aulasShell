e0b092a861f6365dd5c076074d7b40d3629193cb
ozne32 <enzorcc.mf3rs@gmail.com>
2024-07-24 11:48:41 -0300

    aula sobre variaveis

---
+++ apresentacao.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo 'qual é seu nome ?'
+read nome
+echo 'qual é sua idade?'
+read idade
+echo 'qual é a sua cidade ?'
+read cidade
+echo -e "\n Informações do usuário: --Nome: $nome \n --Idade: $idade \n --Cidade: $cidade"
