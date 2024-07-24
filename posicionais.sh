79f54aed2d830bbe0f86c2995db0aa900b2ee34c
ozne32 <enzorcc.mf3rs@gmail.com>
2024-07-24 14:09:55 -0300

    ajustes

--- posicionais.sh
+++ posicionais.sh
@@ -6,13 +6,13 @@ func_help(){
         iniciaNacional para iniciar a aplicação
     "
 }
-func_inicia_nacional(){
-    cd /home/alibabapenguim/nacionalFinal/ #caminho para a sua aplicação docker
+func_inicia_docker(){
+    cd /caminho/para/docker/ #caminho para a sua aplicação docker
     docker-compose up -d
     echo 'está rodando, acesse a aplicação em localhost:8080'
 }
-func_para_nacional(){
-    cd /home/alibabapenguim/nacionalFinal/ #caminho para sua aplicação docker
+func_para_docker(){
+    cd /caminho/para/docker/ #caminho para sua aplicação docker
     docker-compose down
     echo 'containers derrubados'
 }
@@ -22,7 +22,7 @@ fi
 
 case $1 in 
     --help) func_help;;
-    iniciaNacional) func_inicia_nacional;;
-    paraNacional) func_para_nacional;;
+    iniciaDocker) func_inicia_docker;;
+    paraDocker) func_para_docker;;
     *) echo 'digite uma das opções'
 esac
