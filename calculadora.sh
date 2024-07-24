748be061223501769d28d402c089fb83e848324d
ozne32 <enzorcc.mf3rs@gmail.com>
2024-07-24 13:50:56 -0300

    calculadora simples

--- calculadora.sh
+++ calculadora.sh
@@ -1,6 +1,22 @@
-#!bin/sh
-
-n1=1
-n2=2
-res=$n1 -sum $n2
-echo $res
+#!bin/sh 
+echo 'coloque o primeiro número'
+read n1
+echo 'coloque o segundo número'
+read n2
+echo 'qual operação você deseja fazer ?'
+read op
+if [ "$op" = '+' ]; then
+    let res=n1+n2
+    echo "$n1$op$n2=$res"
+elif [ "$op" = '/' ]; then
+    let res=n1/n2
+    echo "$n1$op$n2=$res"
+elif [ "$op" = '-' ]; then
+    let res=n1-n2
+    echo "$n1$op$n2=$res"
+elif [ "$op" = '*' ]; then
+    let res=n1*n2
+    echo "$n1$op$n2=$res"
+else 
+    echo 'insira uma operação válida'
+fi
