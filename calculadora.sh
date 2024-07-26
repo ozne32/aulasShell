0df7c836b85ab0e0f27da4c1897e4fca6ba38216
ozne32 <enzorcc.mf3rs@gmail.com>
2024-07-26 09:25:09 -0300

    jeito melhor de fazer as contas

--- calculadora.sh
+++ calculadora.sh
@@ -6,16 +6,16 @@ read n2
 echo 'qual operação você deseja fazer ?'
 read op
 if [ "$op" = '+' ]; then
-    let res=n1+n2
+    res=`echo "scale=2; $n1 + $n2" | bc`
     echo "$n1$op$n2=$res"
 elif [ "$op" = '/' ]; then
-    let res=n1/n2
+    res=`echo "scale=2; $n1 / $n2" | bc`
     echo "$n1$op$n2=$res"
 elif [ "$op" = '-' ]; then
-    let res=n1-n2
+    res=`echo "scale=2; $n1 - $n2" | bc`
     echo "$n1$op$n2=$res"
 elif [ "$op" = '*' ]; then
-    let res=n1*n2
+    res=`echo "scale=2; $n1 * $n2" | bc`
     echo "$n1$op$n2=$res"
 else 
     echo 'insira uma operação válida'
