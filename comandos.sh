a252fefb479074d61da4b6f0fe5ea23fc3c3755d
ozne32 <enzorcc.mf3rs@gmail.com>
2024-07-26 13:55:50 -0300

    deletando

--- comandos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# > é para sobreescrever o arquivo e >> é para acrescentar
-# $? para pegar o código de saída do ultimo comando 0 --> foi executado com sucesso 127--> não foi executado 
